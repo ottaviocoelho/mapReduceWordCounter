e0a1e8bfe797da432fea616b269277f4403170e1
ottaviocoelho <ottaviocoelho@hotmail.com>
2019-11-12 02:45:09 -0300

    inverted idnex not ready

--- src/main/java/WordCounter/WordCounter/WordCounter.java
+++ src/main/java/WordCounter/WordCounter/WordCounter.java
@@ -2,53 +2,121 @@ package WordCounter.WordCounter;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import java.util.Map;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
 
-public class WordCounter {
-
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer st = new StringTokenizer(value.toString());
+public class WordCounter extends Configured implements Tool {
+    
+    public static class PathMapper extends Mapper<Object, Text, Object, Text> {
+    	
+    	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+    		StringTokenizer st = new StringTokenizer(value.toString());
             Text wordOut = new Text();
-            IntWritable one = new IntWritable(1);
+            FileSplit split = (FileSplit)context.getInputSplit();
+            String path = split.getPath().toString();
+            Text pathText = new Text();
+            pathText.set(path);
             while (st.hasMoreTokens()){
                 wordOut.set(st.nextToken());
-                context.write(wordOut, one);
+                context.write(wordOut, pathText);
             }
-        }
-
+    	}
+    	
     }
-
-    public static class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-        public void reduce(Text term, Iterable<IntWritable> ones, Context context) throws IOException, InterruptedException{
-            int count = 0;
-            Iterator<IntWritable> iterator = ones.iterator();
-            while(iterator.hasNext()) {
-                count++;
-                iterator.next();
-            }
-            IntWritable outpout = new IntWritable((count));
-            context.write(term, outpout);
-        }
-
+    
+    public static class PathReducer extends Reducer<Text, Text, Text, Text> {
+    	
+    	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    		
+    		
+			HashMap<String,Integer> hashtable = new HashMap<String,Integer>();
+		  	Iterator<Text> itr = values.iterator();
+		    int count = 0;
+		    String docID = new String();
+			   
+		    while (itr.hasNext()) {
+	    		docID = itr.next().toString();
+		 
+			    if(hashtable.containsKey(docID)){
+		    	    count = (hashtable.get(docID));
+		    	    count += 1;
+		    	    hashtable.put(docID, count);	
+			    } else {
+			    	hashtable.put(docID, 1);
+			    }
+		    }
+		    
+		    StringBuffer buf = new StringBuffer("");
+			   for(Map.Entry<String, Integer> h: hashtable.entrySet())    
+				buf.append(h.getKey() + ":" + h.getValue() + "\t");
+      	   
+    		Text output = new Text();
+    		output.set(buf.toString());
+    		context.write(key, output);
+    		
+    	}
+    	
     }
+    
+    public class NotepadOutputFormat extends FileOutputFormat<Text, Text> {
+    	
+        @Override
+        public RecordWriter<Text, Text> getRecordWriter(TaskAttemptContext arg0) throws IOException, InterruptedException {
+           Path path = FileOutputFormat.getOutputPath(arg0);
+           Path fullPath = new Path(path, "result.txt");
+	       FileSystem fs = path.getFileSystem(arg0.getConfiguration());
+	       FSDataOutputStream fileOut = fs.create(fullPath, arg0);
+	       return new NotepadWriter(fileOut);
+	    }
+	  }
+    
+	  public class NotepadWriter extends RecordWriter<Text, Text> {
+	      private DataOutputStream out;
+	
+	      public NotepadWriter(DataOutputStream stream) {
+	          out = stream;
+	          try {
+	              out.writeBytes("results:\r\n");
+	          }
+	          catch (Exception ex) {
+	          }  
+	      }
+	
+	      @Override
+	      public void close(TaskAttemptContext arg0) throws IOException, InterruptedException {
+	          out.close();
+	      }
+	
+	      @Override
+	      public void write(Text arg0, Text arg1) throws IOException, InterruptedException {
+	          out.writeBytes(arg0.toString() + ": " + arg1.toString());
+	          out.writeBytes("\r\n");  
+	      }
+	  }
+    
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+    public int run(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 
@@ -58,24 +126,41 @@ public class WordCounter {
         }
         
         System.setProperty("hadoop.home.dir", "/");
+        
 
         Job job = Job.getInstance(conf, "Word Count");
         job.setJarByClass(WordCounter.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setReducerClass(SumReducer.class);
-        job.setNumReduceTasks(10);
+        job.setMapperClass(PathMapper.class);
+        job.setReducerClass(PathReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(10);
+        job.setOutputValueClass(Text.class);
+        job.setOutputFormatClass(NotepadOutputFormat.class);
 
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-        boolean status = job.waitForCompletion(true);
-        if(status) {
-            System.exit(0);
-        } else {
-            System.exit(1);
-        }
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1] + "/temp"));
+        
+        job.waitForCompletion(true);
+        
+        Configuration idConf = new Configuration();
+        Job idJob = Job.getInstance(idConf);
+        
+        idJob.setJarByClass(WordCounter.class);
+        idJob.setOutputKeyClass(Text.class);
+        idJob.setNumReduceTasks(1);
+        idJob.setOutputValueClass(Text.class);
 
+        FileInputFormat.addInputPath(idJob, new Path(otherArgs[1] + "/temp"));
+        FileOutputFormat.setOutputPath(idJob, new Path(otherArgs[0] + "/final"));
+
+        return idJob.waitForCompletion(true) ? 0 : 1;
+        
+    }
+    
+
+    public static void main(String[] args) throws Exception {
+     // TODO Auto-generated method stub
+     ToolRunner.run(new Configuration(), new WordCounter(), args);
     }
 
 }
